35f1b5e8d54db89e4be2e62f67022afb9822e6b9
Sam Morrison <smorrison@wayfair.com>
2019-05-09 09:50:40 -0400

    add copy ability

--- src/measure.js
+++ src/measure.js
@@ -6,6 +6,41 @@ var UI = require('sketch/ui')
 var selectedLayers = document.selectedLayers
 var selectedCount = selectedLayers.length
 
+// Using JSTalk clipboard handling snippet from https://gist.github.com/uhunkler/5465857 by Urs Hunkler
+var clipboard = {
+  // store the pasetboard object
+  pasteBoard : null,
+
+  // save the pasteboard object
+  init : function()
+  {
+    this.pasteBoard = NSPasteboard.generalPasteboard();
+  },
+  // set the clipboard to the given text
+  set : function( text )
+  {
+    if( typeof text === 'undefined' ) return null;
+
+    if( !this.pasteBoard )
+      this.init();
+
+    this.pasteBoard.declareTypes_owner( [ NSPasteboardTypeString ], null );
+    this.pasteBoard.setString_forType( text, NSPasteboardTypeString );
+
+    return true;
+  },
+  // get text from the clipbaoard
+  get : function()
+  {
+    if( !this.pasteBoard )
+      this.init();
+
+    var text = this.pasteBoard.stringForType( NSPasteboardTypeString );
+
+    return text.toString();
+  }
+};
+
 if (selectedCount === 0) {
   context.document.showMessage('No layers are selected.')
 } else {
@@ -13,5 +48,10 @@ if (selectedCount === 0) {
   selectedLayers.forEach(function (layer) {
     totalSurfaceArea += layer.frame.width * layer.frame.height;
   })
-  context.document.showMessage('Total Surface Area: ' + totalSurfaceArea + " square pixels 💥");
-} 
+  if (totalSurfaceArea != "") {
+    context.document.showMessage("Area: " + totalSurfaceArea + "px² 📐 Copied to Clipboard!");
+    clipboard.set("" + totalSurfaceArea);
+  } else {
+    context.document.showMessage("No Area to Copy :(");
+  }
+}
